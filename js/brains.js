41db48331565dd41e7878ea1c3ff011f5fa655d8
Aditya Patel <adityapatel@Adityas-MacBook.local>
2022-10-09 11:50:32 -0400

    added the weather api

--- js/brains.js
+++ js/brains.js
@@ -4,4 +4,28 @@ $(document).ready(function(){
         $(".list-group-item").removeClass("active");
         thisButton.addClass("active");
     })
+    
+    const settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://weatherbit-v1-mashape.p.rapidapi.com/current?lon=-79.3832&lat=43.6532",
+        "method": "GET",
+        "headers": {
+            "X-RapidAPI-Key": "2fcda4957emsh4e87741b3cc4119p1bb358jsn63e9b8c5e378",
+            "X-RapidAPI-Host": "weatherbit-v1-mashape.p.rapidapi.com"
+        }
+    };
+    
+    /*$.ajax(settings).done(function (response) {
+        console.log(response);
+
+        var temp = response.data[0].app_temp;
+        var snow = response.data[0].snow;
+        var rain = response.data[0].precip;
+
+        $(".tempInputSpan").html(temp);
+        $(".snowInputSpan").html(snow);
+        $(".rainInputSpan").html(rain);
+    });*/
+    
 })
