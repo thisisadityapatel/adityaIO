422c833586ba8a45f254a409faebffad7005ff57
Aditya Patel <adityapatel@Adityas-MacBook.local>
2023-01-06 01:34:50 +0530

    minor nav bugs removed

--- js/indexBrains.js
+++ js/indexBrains.js
@@ -107,4 +107,25 @@ $(document).ready(function(){
         $("#sidebarNavExperience").removeClass("show");
         $("#sidebarNavExperience").css("visibility", "hidden");
     })
+
+    const settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://weatherbit-v1-mashape.p.rapidapi.com/current?lon=-79.3832&lat=43.6532",
+        "method": "GET",
+        "headers": {
+            "X-RapidAPI-Key": "2fcda4957emsh4e87741b3cc4119p1bb358jsn63e9b8c5e378",
+            "X-RapidAPI-Host": "weatherbit-v1-mashape.p.rapidapi.com"
+        }
+    };
+    
+    $.ajax(settings).done(function (response) {
+        var temp = response.data[0].temp;
+        var snow = response.data[0].snow;
+        var rain = response.data[0].precip;
+
+        $(".tempInputSpan").html(temp);
+        $(".snowInputSpan").html(snow);
+        $(".rainInputSpan").html(rain);
+    });
 })
